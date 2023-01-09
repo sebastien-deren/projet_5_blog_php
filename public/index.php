658145d77e48ea6c2b82468ea872fc0a4c438139
sebastien deren laptop <sebastien.deren@gmail.com>
2023-01-09 16:01:25 +0100

    #19 ajout du fichier layout + style.css

--- public/index.php
+++ public/index.php
@@ -1,6 +1,15 @@
 <?php
-
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 use Sebastien\Blog\src\Test;
 require_once(dirname(__FILE__).'/../bootstrap.php');
+
+$dir_template = dirname(__FILE__).'/../template/';
+$loader =new FilesystemLoader($dir_template);
+$twig = new Environment($loader,['debug' => true]);
+$twig->addExtension(new \Twig\Extension\DebugExtension);
+$template = $twig->load("layout.html.twig");
 $hello = new Test;
-var_dump($hello);
+$content =" bonjour et bienvenu sur mon site";
+$display= ["title" =>$hello->hello,"content"=>$content];
+echo $template->render($display);
