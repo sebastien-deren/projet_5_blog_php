ac281f8716923df6ef8e6238a0528da73139761b
sebastien deren laptop <sebastien.deren@gmail.com>
2023-02-28 17:02:05 +0100

    correct CommentForm.php

--- src/Form/Abstracts/FormValidifier.php
+++ src/Form/Abstracts/FormValidifier.php
@@ -6,11 +6,27 @@ use Blog\Form\Abstracts\Interface\FormValidifier as InterfaceFormValidifier;
 
 abstract class FormValidifier implements InterfaceFormValidifier
 {
-    protected object $DTO;//i'd like to infer a type here like this object is of type <T> and then when declared boom it's the DTO i want
+    
+    protected object $DTO;// i'd like to infer a type here like this object is of type <T> and then when declared boom it's the DTO i want
+    
+    
+    /**
+     * create a FormValidifier with _POST in $data
+     *
+     * @var array $data this is the $_POST array
+     *
+     * @return void
+     */
     public function __construct(protected array $data)
     {
         
     }
+    
+    /**
+     * We check all our datas and send back a DTO
+     * 
+     * @return object(A DTO) 
+     */
     public function validify():object
     {
         $this->TokenValidation();
@@ -19,6 +35,14 @@ abstract class FormValidifier implements InterfaceFormValidifier
         return $this->DTO;
     }
 
+
+    /**
+     * we check our token received with the token stocked in our server
+     *
+     * @throws Exception
+     *
+     * @return void
+     */
     protected function TokenValidation()
     {
         if (!$this->data['token'] || ($this->data['token'] !== $_SESSION['token'])) {
--- src/Form/Comment/CommentForm.php
+++ src/Form/Comment/CommentForm.php
@@ -10,11 +10,31 @@ use Blog\Form\Abstracts\FormValidifier;
 
 class CommentForm extends FormValidifier
 {
+
+
+    /**
+     * construction of our CommentForm
+     *
+     * @param CreateComment $DTO A DTO object
+     * @param array $data the $_POST data
+     *
+     * @return void
+     */
     public function __construct(CreateComment $DTO, array $data)
     {
         $this->DTO = $DTO;
         parent::__construct($data);
-    }
+
+    }// end __construct
+
+
+    /**
+     * check all the datas from our $_POST
+     *
+     * @return void
+     *
+     * @throws FormException
+     */
     protected function checkingRequired()
     {
         $countline =substr_count( $this->data['content'], "\n" );
@@ -22,16 +42,28 @@ class CommentForm extends FormValidifier
         if (!isset($_SESSION['id'])) {
             throw new FormException("Vous devez être connecter pour poster un commentaire.");
         }
+
         if ($countchar<6) {
             throw new FormException("Vous devez écrire un vrai commentaire (minimum 6 charactère) avant de l'envoyer.");
         }
+
         if(($countline>10)||(($countline>3)&&(($countchar/$countline)<10))){
             throw new FormException("Soyons sérieux trois secondes, ".$countline." ligne dans un commentaire de ".$countchar." characteres.\n Je peux pas accepter ça!");
         }
+
     }
+
+
+    /**
+     * Create The DTO Comment
+     *
+     * @return void
+     */
     protected function createDTO()
     {
         $this->DTO->content = $this->data['content'];
-        $this->DTO->postId = intval($this->data['idPost']);
+        $this->DTO->postId = (int)($this->data['idPost']);
     }
+
+
 }
